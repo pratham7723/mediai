7b2e34650154d60c00f22ef23f1707a7ae50d5bd
pratham7723 <prathammehta773@gmail.com>
2025-02-03 19:47:27 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mediai',
+    version='0.1',
+    author='Pratham mehta',
+    author_email='prathammehta773@gmail.com',
+    packages=find_packages(),
+    install_requires=[''],
+
+)
